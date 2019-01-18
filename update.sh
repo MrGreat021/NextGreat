31bc6cc99e44dcfd0120d347a2e6efe7d92145c7
Aditya021 <46020178+Aditya021@users.noreply.github.com>
2019-01-18 01:43:30 +0000

    Delete update.sh

--- update.sh
+++
@@ -1,9 +0,0 @@
-clear
-apt-get install python
-cd && rm -fr $HOME/SpamCall &&  git clone https://github.com/Aditya021/SpamSms && cd SpamCall
-clear
-cd
-cd SpamCall
-python $HOME/SpamCall/text.py
-echo && ls
-php SpamCall.php
