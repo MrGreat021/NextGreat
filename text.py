457b31f5252e6c3275e084405909c3f16eb21675
Aditya021 <46020178+Aditya021@users.noreply.github.com>
2019-01-18 01:43:54 +0000

    Delete text.py

--- text.py
+++
@@ -1,9 +0,0 @@
-import random
-import sys
-import time
-def mengetik(s):
-    for c in s + '\n':
-        sys.stdout.write(c)
-        sys.stdout.flush()
-        time.sleep(random.random() * 0.1)
-mengetik('Selamat Tools SpamCall Telah Di Update.......') 
