3ac0f86141d14b9ea0a79804e95dece4089215a6
Aditya021 <46020178+Aditya021@users.noreply.github.com>
2019-01-18 01:38:40 +0000

    Add files via upload

---
+++ text.py
@@ -0,0 +1,9 @@
+import random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.1)
+mengetik('Selamat Tools SpamCall Telah Di Update.......') 
